ec9ade5f6ce994139e65777dbe0634ac82f17911
peter88213 <peter.88213@t-online.de>
2023-10-29 10:56:52 +0100

    Update the build structure

--- tools/build_novelyst_yw7.py
+++ tools/build_novelyst_yw7.py
@@ -1,9 +1,9 @@
 """Build a novelyst_yw7 plugin.
         
 In order to distribute a single script without dependencies, 
-this script "inlines" all modules imported from the pywriter package.
+this script "inlines" all modules imported from the novxlib package.
 
-The PyWriter project (see https://github.com/peter88213/PyWriter)
+The novxlib project (see https://github.com/peter88213/novxlib)
 must be located on the same directory level as the novelyst_yw7 project. 
 
 Copyright (c) 2023 Peter Triesberger
@@ -12,7 +12,7 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
 import sys
-sys.path.insert(0, f'{os.getcwd()}/../../PyWriter/src')
+sys.path.insert(0, f'{os.getcwd()}/../../novxlib/src')
 import inliner
 
 SRC = '../src/'
