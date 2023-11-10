d113db6fa8b2cfa6b0b21d923dfbce4c7665be61
peter88213 <peter.88213@t-online.de>
2023-11-10 12:24:53 +0100

    Update project reference to novxlib-Alpha

--- tools/build_novelyst_yw7.py
+++ tools/build_novelyst_yw7.py
@@ -24,7 +24,7 @@ os.makedirs(BUILD, exist_ok=True)
 
 
 def main():
-    inliner.run(SOURCE_FILE, TARGET_FILE, 'novxlib', '../../novxlib/src/')
+    inliner.run(SOURCE_FILE, TARGET_FILE, 'novxlib-Alpha', '../../novxlib/src/')
     print('Done.')
 
 
