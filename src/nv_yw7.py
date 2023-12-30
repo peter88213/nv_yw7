e80fd22b4252c5c4bbb1f52e1d0e1ff55567f632
peter88213 <peter.88213@t-online.de>
2023-12-30 11:55:18 +0100

    Refactor: "ui" --> "view"

--- src/nv_yw7.py
+++ src/nv_yw7.py
@@ -38,15 +38,15 @@ class Plugin:
     _YW_CLASS = Yw7File
     _NOVX_CLASS = NovxFile
 
-    def install(self, model, ui, controller, prefs):
+    def install(self, model, view, controller, prefs):
         """Add commands to the view.
         
         Positional arguments:
             controller -- reference to the main controller instance of the application.
-            ui -- reference to the main view instance of the application.
+            view -- reference to the main view instance of the application.
         """
         self._mdl = model
-        self._ui = ui
+        self._ui = view
         self._ctrl = controller
         self._prefs = prefs
 
