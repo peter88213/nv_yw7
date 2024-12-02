75907fe98f0e37e019892d9e6f4db67d667f6b98
peter88213 <petert88213@gmail.com>
2024-12-02 10:31:36 +0100

    Update docstring.

--- src/nvyw7lib/yw7_purge.py
+++ src/nvyw7lib/yw7_purge.py
@@ -1,7 +1,7 @@
 """Helper module for removing novxlib specific data.
 
 Copyright (c) 2024 Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/nv_yw7
 License: GNU LGPLv3 (https://www.gnu.org/licenses/lgpl-3.0.en.html)
 """
 import re
