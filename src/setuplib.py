4e0dd51eff3302aaaf7eb57e301128478b10edb5
peter88213 <petert88213@gmail.com>
2025-07-06 11:52:51 +0200

    Reformatted the code acc. to PEP-8.

--- src/setuplib.py
+++ src/setuplib.py
@@ -16,7 +16,12 @@ from pathlib import Path
 try:
     from tkinter import *
 except ModuleNotFoundError:
-    print('The tkinter module is missing. Please install the tk support package for your python3 version.')
+    print(
+        (
+            'The tkinter module is missing. '
+            'Please install the tk support package for your python3 version.'
+        )
+    )
     sys.exit(1)
 
 PLUGIN = 'nv_yw7.py'
@@ -83,9 +88,21 @@ def main(zipped=True):
         # Install the localization files.
         output(f'Copying locale ...')
         copy_tree('locale', applicationDir)
-        output(f'Sucessfully installed "{PLUGIN}" at "{os.path.normpath(pluginDir)}"')
+
+        # Show a success message.
+        output(
+            (
+                f'Sucessfully installed "{PLUGIN}" '
+                f'at "{os.path.normpath(pluginDir)}".'
+            )
+        )
     else:
-        output(f'ERROR: Cannot find a novelibre installation at "{applicationDir}"')
+        output(
+            (
+                'ERROR: Cannot find a novelibre installation '
+                f'at "{os.path.normpath(applicationDir)}".'
+            )
+        )
 
     root.quitButton = Button(text="Quit", command=quit)
     root.quitButton.config(height=1, width=30)
